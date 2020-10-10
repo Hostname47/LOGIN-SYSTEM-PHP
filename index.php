d86615d0e95dcbe81e75e2e8ff9f46c19a2b5bac
HOSTNAME47 <mouadstev1@gmail.com>
2020-10-10 21:03:41 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="styles/sign-in.css">
+</head>
+<body>
+    <header>
+        <div id="top-header-strip">
+            <h2>Time will show who was the hardest worker in the room ..</h2>
+        </div>
+    </header>
+
+    <main>
+        
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
